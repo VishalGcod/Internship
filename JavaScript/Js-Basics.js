f8c805fd00b09f731de0cad362f0a60693b472bd
VishalGcod <gvishal17102000@gmail.com>
2023-06-28 17:34:56 +0530

    JS  28/6/23

--- JavaScript/Js-Basics.js
+++ JavaScript/Js-Basics.js
@@ -1,4 +1,4 @@
-// console.log();
+// console.log();     numbers
 // var no1 = 1;
 // var no2 = 2;
 // var sum = no1 + no2;
@@ -21,5 +21,52 @@
 // console.log(parseInt(num3))
 // console.log(num3.toString())
 // console.log(parseInt(num5))
+// var strFloat=87.9870456
+// console.log(strFloat.toFixed())
+// console.log(strFloat.toFixed(2))
+// console.log(strFloat.toFixed(6))
+// console.log(strFloat.toFixed(7))
 
+// strings
+// var str1='i love js'
+// console.log(typeof(str1))
+// var str2='i \'love\' js'
+// console.log(str2)
+// var str3="i \"love\" js"
+// console.log(str3)
+// var str4='i "love" js'
+// var str5="i 'love' js"
+// console.log(str4)
+// console.log(str5)
 
+// string function
+
+// var str1='i love js '
+// console.log(str1.length)
+// console.log(str1.indexOf('i'))
+// console.log(str1.lastIndexOf('i'))
+// console.log(str1.slice(0,5));
+// console.log(str1.slice(-5));
+// console.log(str1.slice(6));
+
+// console.log(str1.substr(2,5));
+// console.log(str1.toLocaleLowerCase());
+// console.log(str1.toLocaleUpperCase());
+
+// concat
+
+// var str1='java'
+// var str2='script'
+// console.log(str1.concat(" "+str2));
+// console.log(str1+" "+str2);
+
+// trim
+var str1 = "    ,java script,    ";
+console.log(str1.trim());
+
+// charat
+console.log(str1.charAt(6));
+
+// split
+console.log(str1.split(" "));
+console.log(str1.split(","));
