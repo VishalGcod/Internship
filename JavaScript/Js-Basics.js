ae2842e1c12b87b18f8999fb38da8dde0c59a46c
VishalGcod <gvishal17102000@gmail.com>
2023-06-28 17:05:13 +0530

    JS  28/6/23

---
+++ JavaScript/Js-Basics.js
@@ -0,0 +1,25 @@
+// console.log();
+// var no1 = 1;
+// var no2 = 2;
+// var sum = no1 + no2;
+// console.log(typeof (0 / 0));
+// console.log(0 / 5);
+// console.log(3 * "a");
+// console.log("a" * 3);
+// console.log(5 - "c");
+// console.log(5 + "v");
+// console.log("v" + 5);
+// console.log(sum);
+// var num1 = 10;
+// var num2='10'
+// var num3='11.1'
+// var num4=11.1
+// var num5='abc'
+// console.log(num1.toString())
+// console.log(parseInt(num2))
+// console.log(parseFloat(num3))
+// console.log(parseInt(num3))
+// console.log(num3.toString())
+// console.log(parseInt(num5))
+
+
